5a8c598cbf9fe0dfb30cbed2a8ce60fecae95a49
ildarkamalov <ildar.kamalov15@gmail.com>
2013-04-04 18:46:22 +0400

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kino3::Application.config.secret_token = '0e83d466c0c2b37a5f93fb3e7b4845562569257465fff32356e313ce7c91d01aae9bfa2b544c1e17a05458b16e21052877ace1e8abbed6b0eec1595922eb6a99'
