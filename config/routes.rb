e760de2b1383e70ddce89451164aca74c9488ddb
ildarkamalov <ildar.kamalov15@gmail.com>
2013-04-25 17:57:28 +0400

    resource User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Kino3::Application.routes.draw do
+  get "home/index"
+
+  devise_for :users
+
   resources :cinemas
 
 
@@ -54,7 +58,7 @@ Kino3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
